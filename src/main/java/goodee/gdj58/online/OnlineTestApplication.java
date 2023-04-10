441522a160d06eb15ca5800e8bb1e08d058f0bcc
eunseochoi <0602dmstj@naver.com>
2023-04-10 15:52:14 +0900

    test

--- src/main/java/goodee/gdj58/online/OnlineTestApplication.java
+++ src/main/java/goodee/gdj58/online/OnlineTestApplication.java
@@ -2,12 +2,18 @@ package goodee.gdj58.online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
 public class OnlineTestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources((OnlineTestApplication.class));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineTestApplication.class, args);
